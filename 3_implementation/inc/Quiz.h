a30752402920303cdf11c349efa6b73a20306252
Tamil-durga-Pari <94234015+Tamil-durga-Pari@users.noreply.github.com>
2021-11-25 13:31:04 +0530

    Update Quiz.h

--- 3_implementation/inc/Quiz.h
+++ 3_implementation/inc/Quiz.h
@@ -1,1 +1,18 @@
 
+// Quiz game header file
+
+#include<stdio.h>
+#include<conio.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<string.h>
+void show_record();
+void reset_score();
+void help();
+void edit_score(float , char []);
+int main()
+     {
+     int countr,r,r1,count,i,n;
+     float score;
+     char choice;
+     char playername[20];
